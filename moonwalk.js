1542f49aea39b594e80572b0ae70e0baab4519c8
Nobita <aurelio.ogliari@gmail.com>
2014-06-02 17:18:22 +0100

    refactored javascript

--- moonwalk.js
+++ moonwalk.js
@@ -6,18 +6,21 @@
 
 		var nav = document.getElementById('Moonwalk-nav'),
 			 toggler = document.getElementById('Moonwalk-toggle'),
+			 cnt = document.getElementById('Moonwalk-cnt'),
 			 wideClass = 'nav-moonwalk-full',
 			 toggleClass = 'nav-mobile-open';
-
 		var init = function(){
+			// mouse events are easy to detect and they're directly sent
+			// to their relevant function.
 			addEvt(nav, 'mouseover', 'add', wideClass);
 			addEvt(nav, 'mouseleave', 'remove', wideClass);
 			addEvt(nav, 'click', 'toggle', toggleClass);
 		},
 		addEvt = function(el, evt, action, classNm){
-			el.addEventListener(evt, function(){
-				document.body.classList[action](classNm);
-			}, false);
+				el.addEventListener(evt, classController.bind(null, action, classNm), false);				
+		},
+		classController = function(action, classNm){
+			document.body.classList[action](classNm);
 		};
 
 		return {
