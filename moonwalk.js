5fa57f880f1b41b52d9af41fe3270b2fc632a2ea
Nobita <aurelio.ogliari@gmail.com>
2014-06-02 22:03:42 +0100

    added touch class support & user options

--- moonwalk.js
+++ moonwalk.js
@@ -2,7 +2,14 @@
 	
   "use strict";
 
-	var MoonwalkNav = (function(opts){
+  	var Utils = {
+		// check if touch is supported
+  		// see: http://stackoverflow.com/a/15439809/1446845
+  		isTouch : (('ontouchstart' in window) || (navigator.msMaxTouchPoints > 0))
+  	};
+
+
+	var MoonwalkNav = (function(){
 
 		var nav = document.getElementById('Moonwalk-nav'),
 			 toggler = document.getElementById('Moonwalk-toggle'),
@@ -10,17 +17,31 @@
 			 openClass = 'nav-moonwalk-full',
 			 mobileOpenClass = 'nav-mobile-open',
 			 mobileActivatedClass = 'nav-mobile-on',
-			 mobileMenuOpen = true;
+			 i,
+			 // Defaults, overridable by the user
+			 settings = {
+			 	addTouchClass : false,
+			 	touchClass : 'moonwalk-touch'
+			 };
 
-		var init = function(){
+		var init = function(opts){
 			// Close Mobile menu (if js is disabled the nav will still be visible)
 			_classController('add', mobileActivatedClass);
-			mobileMenuOpen = false;
 			// mouse events are easy to detect and they're directly sent
 			// to their relevant function.
 			_addEvt(nav, 'mouseover', _openNav);
 			_addEvt(nav, 'mouseleave', _closeNav);
 			_addEvt(toggler, 'click', _toggleMobileNav);
+			// if options are passed override default settings
+			if(opts){ _updateOpts(opts); }
+			// if addTouchClass is set to true add the touch class 
+			_classController('add', settings.touchClass);
+		},
+		_updateOpts = function(opts){
+			// iterate over the options passed and override default settings
+			for ( i in opts ) {
+				settings[i] = opts[i];
+			}
 		},
 		_openNav = function(){
 			_classController('add', openClass);
@@ -29,9 +50,7 @@
 			_classController('remove', openClass);
 		},
 		_toggleMobileNav = function(){
-
 			_classController('toggle', mobileOpenClass);
-	
 		},
 		_addEvt = function(el, evt, callback){
 				el.addEventListener(evt, callback, false);				
